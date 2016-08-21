f1331c3be4b0fb92ce91cf4176ea0d21e72f3ce9
phabos <prezatf@gmail.com>
2016-08-21 13:42:35 +0200

    nc

--- controllers/artistsController.js
+++ controllers/artistsController.js
@@ -3,12 +3,10 @@ var Artist = require( __dirname + '/../models/artist');
 var ArtistAlbum = require( __dirname + '/../models/artistAlbum');
 
 var artists = {
-    // Homepage
     home: function( reply ) {
       // Display home
       return reply.view('home_artists');
     },
-    // Insert artist endpoint
     insert: function( request, reply ) {
       var artist = new Artist({ name: request.payload.artistName, uniqid: uniqid() });
       artist.save(function(err) {
@@ -16,28 +14,24 @@ var artists = {
         return reply({ error: 0, msg: 'artist inserted' }).code( 200 );
       });
     },
-    // List artists endpoint
     get: function( reply ) {
       Artist.find({}).sort('name').exec( function(err, artistList) {
         if (err) throw err;
         return reply( artistList );
       });
     },
-    // Get artist DETAIL
     artist: function( request, reply ) {
       Artist.findOne({ _id: request.params.id }).exec( function(err, artistDetail) {
         if (err) throw err;
         return reply.view( 'artist', { artist: artistDetail } );
       });
     },
-    //Get Album details
     albums: function( request, reply ) {
       ArtistAlbum.find({ artistId: request.params.id }).exec( function(err, albumDetail) {
         if (err) throw err;
         return reply( albumDetail );
       });
     },
-    // Insert artist endpoint
     albumInsert: function( request, reply ) {
       // Validation
       var album = new ArtistAlbum({ name: request.payload.albumName, artistId: request.payload.artistId, list: request.payload.albumList});
@@ -46,7 +40,6 @@ var artists = {
         return reply({ error: 0, msg: 'album inserted' }).code( 200 );
       });
     },
-    // delete
     delete: function( request, reply ) {
       Artist.remove({ _id: request.params.id }, function(err,removed) {
         if (err) throw err;
